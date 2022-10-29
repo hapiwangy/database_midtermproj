fcd3ae489c774637c6c36b93be4112cee5eac787
happy_m <happyhaha@g.ncu.edu.tw>
2022-10-29 21:51:53 +0800

    10/29測試完可以登入

---
+++ test.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+import os
+import pprint
+
+# path = os.getcwd() + r"\data"
+# mongod = r"C:\Users\yudan\Downloads\mongodb-win32-x86_64-windows-6.0.2\bin\mongod.exe"
+
+# os.system(f"{mongod} --dbpath={path}")
+client = MongoClient(host="localhost", port=27017)
+
+db = client.banned
+x = "accountname"
+y = "password"
+for STs in db.user.find():
+    print(STs)
